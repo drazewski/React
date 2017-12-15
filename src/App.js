db2c1a672a4f77cecede31e6aa71566ef08ba13c
Łukasz Drążewski <lukasz.drazewski@performgroup.com>
2017-12-15 17:14:54 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      deadline: '25 grudnia 2018',
+      newdeadline: ''
+    }
+  }
+
+  changeDealine() {
+    this.setState({
+      newdeadline: event.target.value
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h2>Odliczanie do dnia: {this.state.deadline}</h2>
+        <div>
+          <p className="Clock-days">Dni: 100</p>
+          <p className="Clock-hours">Godzin: 12</p>
+          <p className="Clock-minutes">Minut: 17</p>
+          <p className="Clock-seconds">Sekund: 55</p>
+        </div>
+        <input type="text" placeholder="Wpisz nową datę" className=".Deadline-input" />
+        <button onClick={() => this.changeDealine()} >Zatwierdz </button>
+      </div>
+    )
+  }
+}
+
+export default App
